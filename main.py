5e6b9ee1bdef898d532dbdbf2447855323f44dae
nainasah0610 <nainasah0610@gmail.com>
2026-04-28 08:38:48 +0530

    main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.write("Hello! Creating a simple web application using streamlit library")
 #Text input:
 name= st.text_input("Enter your name: ")
 #Number input:
-age=st.number_input("Enter your age: )
+age=st.number_input("Enter your age: ")
 
 #Display a message when the button is clicked:
 if st.button("Submit"):
